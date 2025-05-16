469c29673b18a37b593096a56434e464e7328091
X_Raxs <113223781+XRaxs@users.noreply.github.com>
2025-05-17 01:45:35 +0700

    masukin ke pages

--- src/index.js
+++ src/index.js
@@ -88,8 +88,9 @@ document.querySelector('.skip-link').addEventListener('click', (e) => {
 });
 
 if ('serviceWorker' in navigator) {
+  const baseURL = document.querySelector('base')?.getAttribute('href') || './';
   window.addEventListener('load', () => {
-    const swPath = `service-worker.js`;
+    const swPath = `${baseURL}service-worker.js`;
 
     navigator.serviceWorker.register(swPath)
       .then(reg => console.log('Service Worker registered:', reg))
